c98b274ce05e265cb186883e630ff1c4781e9fbe
Suparno Kanto Banerjee <sbaner48@its.jnj.com>
2015-04-26 23:49:36 +0200

    jnjb2bfulfilmentprocess extension check in

---
+++ global/jnj-b2b-accelerator/jnjb2btest/src/com/jnj/b2b/test/constants/Jnjb2bTestConstants.java
@@ -0,0 +1,42 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *  
+ */
+package com.jnj.b2b.test.constants;
+
+/**
+ * Global class for all Jnjb2bTest constants. You can add global constants for your extension into this class.
+ */
+public final class Jnjb2bTestConstants 
+{
+	public static final String EXTENSIONNAME = "jnjb2btest";
+
+	public static final String PAY_USER_UID = "qapayuser@test.com";
+	public static final String APPROVER_UID = "qaapprover@test.com";
+	public static final String QUOTE_USER_UID = "qaquoteuser@test.com";
+	public static final String HISTORICAL_USER_UID = "qaorderhistoryuser@test.com";
+	public static final String REPENISHMENT_USER_UID = "qareplenishmentuser@test.com";
+
+	public static final String LOWLIMIT_COSTCENTER = "QAEndCostCenter";
+	public static final String STANDARD_COSTCENTER = "QAMiddleCostCenter";
+
+	public static final String ACCOUNTMANAGERUID = "QAAccMnr";
+
+	public static final String POWERTOOLS_SITE = "powertools";
+
+
+
+	private Jnjb2bTestConstants()
+	{
+		//empty
+	}
+}
