9bf40be633c2f3a7018b16c80751b78641f6bae2
Suparno Kanto Banerjee <sbaner48@its.jnj.com>
2015-04-27 12:38:48 +0200

    first add to git.

---
+++ global/jnj-b2b-accelerator/jnjb2bfulfilmentprocess/gensrc/com/jnj/b2b/fulfilmentprocess/constants/GeneratedJnjb2bFulfilmentProcessConstants.java
@@ -0,0 +1,33 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Apr 26, 2015 2:49:18 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.jnj.b2b.fulfilmentprocess.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedJnjb2bFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "jnjb2bfulfilmentprocess";
+	public static class Attributes
+	{
+		public static class ConsignmentProcess
+		{
+			public static final String DONE = "done".intern();
+			public static final String WAITINGFORCONSIGNMENT = "waitingForConsignment".intern();
+			public static final String WAREHOUSECONSIGNMENTSTATE = "warehouseConsignmentState".intern();
+		}
+	}
+	
+	protected GeneratedJnjb2bFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
