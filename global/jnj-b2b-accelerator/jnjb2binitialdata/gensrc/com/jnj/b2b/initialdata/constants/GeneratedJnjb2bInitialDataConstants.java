9a3f27e1cc31e0e3245ae77e28feddd0f706915a
Suparno Kanto Banerjee <sbaner48@its.jnj.com>
2015-04-27 12:48:13 +0200

    first add to git.

---
+++ global/jnj-b2b-accelerator/jnjb2binitialdata/gensrc/com/jnj/b2b/initialdata/constants/GeneratedJnjb2bInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Apr 21, 2015 8:15:22 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.jnj.b2b.initialdata.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedJnjb2bInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "jnjb2binitialdata";
+	
+	protected GeneratedJnjb2bInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ global/jnj-b2b-accelerator/jnjb2binitialdata/src/com/jnj/b2b/initialdata/constants/Jnjb2bInitialDataConstants.java
@@ -0,0 +1,28 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *  
+ */
+package com.jnj.b2b.initialdata.constants;
+
+/**
+ * Global class for all Jnjb2bInitialData constants. You can add global constants for your extension into this
+ * class.
+ */
+public final class Jnjb2bInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "jnjb2binitialdata";
+
+	private Jnjb2bInitialDataConstants()
+	{
+		//empty
+	}
+}
