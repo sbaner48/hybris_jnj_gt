75cf3b147ea3d0deb766eee759b4ea5509c2c46c
Suparno Kanto Banerjee <sbaner48@its.jnj.com>
2015-04-27 12:31:50 +0200

    first add to git.

---
+++ global/jnj-b2b-accelerator/jnjb2bfacades/gensrc/com/jnj/b2b/facades/constants/GeneratedJnjb2bFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Apr 26, 2015 2:49:18 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.jnj.b2b.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedJnjb2bFacadesConstants
+{
+	public static final String EXTENSIONNAME = "jnjb2bfacades";
+	
+	protected GeneratedJnjb2bFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
