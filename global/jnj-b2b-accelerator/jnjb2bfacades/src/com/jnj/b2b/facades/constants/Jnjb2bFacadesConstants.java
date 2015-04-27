a9ef02c916a3d7ee4f4c9477c5c6d9c0e48211f3
Suparno Kanto Banerjee <sbaner48@its.jnj.com>
2015-04-27 12:12:59 +0200

    first add to git.

---
+++ global/jnj-b2b-accelerator/jnjb2bfacades/src/com/jnj/b2b/facades/constants/Jnjb2bFacadesConstants.java
@@ -0,0 +1,30 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *  
+ */
+package com.jnj.b2b.facades.constants;
+
+/**
+ * Global class for all B2BAcceleratorFacades constants. You can add global constants for your extension into this
+ * class.
+ */
+@SuppressWarnings("PMD")
+public class Jnjb2bFacadesConstants extends GeneratedJnjb2bFacadesConstants
+{
+	public static final String EXTENSIONNAME = "jnjb2bfacades";
+
+	private Jnjb2bFacadesConstants()
+	{
+		super();
+		assert false;
+	}
+}
